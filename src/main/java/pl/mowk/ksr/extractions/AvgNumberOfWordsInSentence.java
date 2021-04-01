ffbd644ef27914ac7f3d724892e2246693e09221
Wojciech Kulesza <Ptpiwi@gmail.com>
2021-04-01 21:03:39 +0200

    AvgNumberOfWordsInSentence implemented

--- src/main/java/pl/mowk/ksr/extractions/AvgNumberOfWordsInSentence.java
+++ src/main/java/pl/mowk/ksr/extractions/AvgNumberOfWordsInSentence.java
@@ -1,6 +1,23 @@
 package pl.mowk.ksr.extractions;
 
+import pl.mowk.ksr.data.Article;
+
 public class AvgNumberOfWordsInSentence {
-    //todo implement
+    public static double countSentences(Article article){
+        String[] segments = article.getBody().split("\\.");
+        double tmp= 0;
+        for (String sentence:
+                segments
+             ) {
+            char[] ch = new char[sentence.length()];
+            for(int i=0;i<sentence.length();i++)
+            {
+                ch[i]= sentence.charAt(i);
+                if( ((i>0)&&(ch[i]!=' ')&&(ch[i-1]==' ')) || ((ch[0]!=' ')&&(i==0)) )
+                    tmp++;
+            }
+        }
+        return tmp/segments.length;
+    }
     
 }
